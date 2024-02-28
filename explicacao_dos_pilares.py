e7f24322b4771344af09ac2a19f6886da498360c
léo <lpoliveira132@gmail.com>
2024-02-28 19:10:44 -0300

    expĺicacao sobre heranca, encapsulamento, polimorfismo e abstracao

---
+++ explicacao_dos_pilares.py
@@ -0,0 +1,9 @@
+#herança-> herda coisas por exemplo civic, classic e gol sao carros entao eles podem herdar os metodos do carro que ja faz tudo isso
+
+#encapsulamento-> vc nao precissa saber muito sobre oq esta dentro do objeto por exemplo a tv vc liga ela com controle remoto,
+# e faz tudo por ali, e vc nem precisa saber quais sao as peças da tv
+
+#polimorfismo-> substituir tipoas para se comportar do mesmo jeito
+
+#abstraído-> saber oq abstrair por exemplo o cliente de um hospital nao é o mesmo cliente de uma mecanica, os atributos de interesse serao diferentes
+
